354255739c2fd0655f10d6928690e5e4b49ca597
root <logicmoo@gmail.com>
2017-02-18 12:51:21 -0800

    removed a breaking provides/1

--- pack.pl
+++ pack.pl
@@ -7,4 +7,4 @@ packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 home( 'https://github.com/TeamSPoon/file_scope' ).
 download( 'https://github.com/TeamSPoon/file_scope/release/*.zip' ).
-provides(set_prolog_flag_until_eof/2).
+
