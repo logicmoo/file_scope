4d40e2e383949de6ad629ee308a914be4e497044
root <logicmoo@gmail.com>
2017-03-09 15:05:16 -0800

    v1.1.112

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(file_scope).
-version('1.1.111').
+version('1.1.112').
 title('File local scoped efects').
 keywords([source]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
@@ -8,5 +8,6 @@ maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
 home( 'https://github.com/TeamSPoon/file_scope' ).
 download( 'https://github.com/TeamSPoon/file_scope/release/*.zip' ).
 requires(must_trace).
+requires(logicmoo_utils).
 autoload(true).
 
